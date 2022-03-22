a5c119a1b18573eedfcee32de24dec125703c3ca
lab596 <86579136+lab596@users.noreply.github.com>
2022-03-21 19:57:52 -0700

    Create CamaraMovesInIntro.java

---
+++ methods/CamaraMovesInIntro.java
@@ -0,0 +1,6 @@
+ 
+public void CameraMovesInIntro() {     
+  this.camera.moveAndOrientTo( this.StartOfIntro, MoveAndOrientTo.duration( 1.0 ) );     
+  this.camera.moveAndOrientTo( this.Intro2, MoveAndOrientTo.duration( 1.0 ) );     
+  this.camera.moveAndOrientTo( this.Intro3, MoveAndOrientTo.duration( 1.0 ) );  
+}
