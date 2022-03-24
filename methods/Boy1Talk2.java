5b040368c1bca8bc1fff2ddb136b1f4e7e73cc60
lab596 <86579136+lab596@users.noreply.github.com>
2022-03-23 20:28:26 -0700

    Create Boy1Talk2.java

---
+++ methods/Boy1Talk2.java
@@ -0,0 +1,20 @@
+public void Boy1Talk2() {     
+  doTogether( () -> {         
+    this.teenPerson9.say( "Remember mom, I'm going to the ice cream shop with some of my friends after school, then we are heading to the game.", Say.duration( 6.0 ) );     
+  }, () -> {         
+    this.playAudio( new AudioSource( Resources.Remember_Mom_mp3 ) );     
+  }, () -> {         
+    this.teenPerson9.talk();         
+    this.teenPerson9.talk();         
+    this.teenPerson9.talk();         
+    this.teenPerson9.talk();         
+    this.teenPerson9.talk();         
+    this.teenPerson9.talk();         
+    this.teenPerson9.talk();         
+    this.teenPerson9.talk();         
+    this.teenPerson9.talk();         
+    this.teenPerson9.talk();         
+    this.teenPerson9.talk();         
+    this.teenPerson9.talk();     
+  } );  
+}
